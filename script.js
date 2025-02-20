bb90cefa512be52e2dc79d0a72b6f883125de57e
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-20 23:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	var res = ""
+    var charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for(var i = 0; i<l; i++){
+        res += charList.charAt(Math.floor(Math.random()*charList.length))
+    }
+    return res;
 }
 
 // Do not change the code below.
